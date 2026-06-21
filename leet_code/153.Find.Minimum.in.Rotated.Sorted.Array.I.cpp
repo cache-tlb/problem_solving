ea486b488349e7a77be5039d445d402230223346
cache-tlb <liubin1991@outlook.com>
2026-06-21 18:13:41 +0800

    Create 153.Find.Minimum.in.Rotated.Sorted.Array.I.cpp

---
+++ leet_code/153.Find.Minimum.in.Rotated.Sorted.Array.I.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int find(int *a, int n) {
+        if (n == 1) return a[0];
+        if (n == 2) return std::min(a[0], a[1]);
+        int p = n / 2;
+        if (a[0] < a[p]) return std::min(a[0], find(a+p, n-p));
+        else /*if (a[0] > a[p])*/ return find(a, p+1);
+    }
+    int findMin(vector<int>& nums) {
+        return find(&nums[0], nums.size());
+    }
+};
